bae5430889fd800e60b370270d5cdcdcfa521301
kefasogabi <kefasogabi@gmail.com>
2020-05-21 17:19:36 +0100

    Initial

---
+++ Exercise03/Decryption.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class Decryption
+    {
+        public void DecryptFile(string filePath, string key)
+        {
+
+            byte[] encrypted = File.ReadAllBytes(filePath);
+            using (var DES = new DESCryptoServiceProvider())
+            {
+                DES.IV = Encoding.UTF8.GetBytes(key);
+                DES.Key = Encoding.UTF8.GetBytes(key);
+                DES.Mode = CipherMode.CBC;
+                DES.Padding = PaddingMode.PKCS7;
+
+                using (var memStream = new MemoryStream())
+                {
+
+                    CryptoStream cryptoStream = new CryptoStream(memStream, DES.CreateDecryptor(),
+                        CryptoStreamMode.Write);
+                    cryptoStream.Write(encrypted, 0, encrypted.Length);
+
+                    cryptoStream.FlushFinalBlock();
+                    File.WriteAllBytes(filePath, memStream.ToArray());
+
+                    Console.WriteLine("Decrypted succesfully " + filePath);
+                }
+            }
+        }
+    }
+}
---
+++ Exercise03/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var path = @"C:\Test\card.xml";
+            var key = "hwxkwtyp";
+
+
+            try
+            {
+                var decrypt = new Decryption();
+
+                decrypt.DecryptFile(path, key);
+
+
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
